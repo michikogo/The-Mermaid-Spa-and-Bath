70b2567e02257c6c61146dfb29b69d149556542c
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-10 19:57:35 +0800

    changed routes to match the url

--- src/Routes.js
+++ src/Routes.js
@@ -12,14 +12,14 @@ import Reservations from './pages/Reservations';
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <Route exact path='/The-Mermaid-Spa-and-Bath/#/' component={Home} />
-      <Route path='/The-Mermaid-Spa-and-Bath/#/about' component={About} />
-      <Route path='/The-Mermaid-Spa-and-Bath/#/products' exact component={Products} />
-      <Route path='/The-Mermaid-Spa-and-Bath/#/products/:id' component={ProductSpecific} />
-      <Route path='/The-Mermaid-Spa-and-Bath/#/treatments' component={Treatments} />
-      <Route path='/The-Mermaid-Spa-and-Bath/#/promo' component={Promo} />
-      <Route path='/The-Mermaid-Spa-and-Bath/#/reservations' component={Reservations} />
+    <HashRouter>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/products' exact component={Products} />
+      <Route path='/products/:id' component={ProductSpecific} />
+      <Route path='/treatments' component={Treatments} />
+      <Route path='/promo' component={Promo} />
+      <Route path='/reservations' component={Reservations} />
 
     </HashRouter>
   );
