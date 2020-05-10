42355d9d86184418f0fb19738d60ee2f0505dd56
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-10 15:58:15 +0800

    Updates

---
+++ precache-manifest.9cf43c0fa2c71e64ae08283ce951285c.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22bd701c8e9151d2621044e67ab2b5a",
+    "url": "/The-Mermaid-Spa-and-Bath/index.html"
+  },
+  {
+    "revision": "3d1cf55171af9bddf229",
+    "url": "/The-Mermaid-Spa-and-Bath/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ddb640db7cf041f5bc7e",
+    "url": "/The-Mermaid-Spa-and-Bath/static/css/main.567c3ed5.chunk.css"
+  },
+  {
+    "revision": "3d1cf55171af9bddf229",
+    "url": "/The-Mermaid-Spa-and-Bath/static/js/2.4928925b.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/The-Mermaid-Spa-and-Bath/static/js/2.4928925b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb640db7cf041f5bc7e",
+    "url": "/The-Mermaid-Spa-and-Bath/static/js/main.51b4b3c1.chunk.js"
+  },
+  {
+    "revision": "57ffca125c8fa651f457",
+    "url": "/The-Mermaid-Spa-and-Bath/static/js/runtime-main.01a076fe.js"
+  },
+  {
+    "revision": "672524377ab6ea5ea7bf2a1ad1f1ade4",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/about.67252437.png"
+  },
+  {
+    "revision": "39c82fbfcaa9d505c49863b805800c1f",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/about2.39c82fbf.png"
+  },
+  {
+    "revision": "ea07edba97a85558cf4eacfdf9084fc4",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract3.ea07edba.png"
+  },
+  {
+    "revision": "f5a22e3618029c426062ab10b42ec880",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract4.f5a22e36.png"
+  },
+  {
+    "revision": "9b07f0a459714bc3bba0cef9e92560b6",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract5-flip.9b07f0a4.png"
+  },
+  {
+    "revision": "d8bd2941a4ce6cbf84c06da0728ec1e0",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract5.d8bd2941.png"
+  },
+  {
+    "revision": "55c8a833a158be4f78fb377a420f5527",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract6.1.55c8a833.png"
+  },
+  {
+    "revision": "778cc1da1a6f6822e073956e339e50f0",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract6.2.778cc1da.png"
+  },
+  {
+    "revision": "93064706e50532feba727ce635be3042",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract7.93064706.png"
+  },
+  {
+    "revision": "720fa23a7de8ddb14577df2a17051f6c",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/abstract8.720fa23a.png"
+  },
+  {
+    "revision": "4480134e7a4db57712fe8399e1852e6a",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/check.4480134e.jpg"
+  },
+  {
+    "revision": "6e48868c5b29804c8aae62bb71ba368c",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/logo.6e48868c.png"
+  },
+  {
+    "revision": "943540d9033f309c1188c1559eeea42c",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product1.943540d9.jpg"
+  },
+  {
+    "revision": "3a9c546b20ec04b06573397fe43a6977",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product10.3a9c546b.jpg"
+  },
+  {
+    "revision": "fd2a0948d92e43d83bb631e9843c5fb4",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product2.fd2a0948.jpg"
+  },
+  {
+    "revision": "c784552bc4bd438b9287f79a03c6fdbb",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product3.c784552b.jpg"
+  },
+  {
+    "revision": "3e38651e8829c8df5115352b4b56b799",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product4.3e38651e.jpg"
+  },
+  {
+    "revision": "cc1a08b22ce16aa8aa62b6279e01b69d",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product5.cc1a08b2.jpg"
+  },
+  {
+    "revision": "712890d8e0d14b94a4e4cc95cba08db9",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product6.712890d8.jpg"
+  },
+  {
+    "revision": "38dd9ded376904aae32baf786b46672d",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product7.38dd9ded.jpg"
+  },
+  {
+    "revision": "a74fa919e436947f69541d7685aeab23",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product8.a74fa919.jpg"
+  },
+  {
+    "revision": "0873e4cec258799834dd6fe93558eaae",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/product9.0873e4ce.jpg"
+  },
+  {
+    "revision": "293d41c7b77a74caeee5297a2e024787",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/products.293d41c7.jpg"
+  },
+  {
+    "revision": "b75022f0a517f51b7f159bf1b09f796c",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/promo.b75022f0.jpg"
+  },
+  {
+    "revision": "c95688f4f7eec70179a6b5dc00bc9d06",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/reservation.c95688f4.jpg"
+  },
+  {
+    "revision": "1c6d20fd2e95c0ed394927962b8af70c",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/spa1.1c6d20fd.jpeg"
+  },
+  {
+    "revision": "4884bcc014c8fae82c396c7648cf1348",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/spa2.4884bcc0.jpg"
+  },
+  {
+    "revision": "2630e5f2b83d5afebb02d2d6b96bf373",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/spa3.2630e5f2.jpg"
+  },
+  {
+    "revision": "7e5491a26bace2a5eb4e5f9e73f5d3ff",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/spa4.7e5491a2.jpg"
+  },
+  {
+    "revision": "1aa823e700e27b978a513ae4f103a0e7",
+    "url": "/The-Mermaid-Spa-and-Bath/static/media/staff.1aa823e7.jpg"
+  }
+]);
