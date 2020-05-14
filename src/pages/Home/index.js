bcb8e4900fe3e5727f2ea852e380f0540c2cb131
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-14 19:38:21 +0800

    updated url for home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,15 +14,15 @@ import Footer from '../../common/footer';
 class Home extends Component {
     render() {
         const ppt = [
-            { image: spa1, title: "Swiddish Massage", description: "Swedish massage typically covers the whole body with firm strokes but without much deep, focused work." },
+            { image: spa1, title: "Swedish Massage", description: "Swedish massage typically covers the whole body with firm strokes but without much deep, focused work." },
             { image: spa2, title: "Hot Stone Massage", description: "Hot stone massage uses smooth, rounded basalt stones that have been heated in water and retains their heat well. The heat helps warm up your muscles and is very relaxing." },
             { image: spa3, title: "Facial", description: "Facials have the same basics steps: Cleanse, exfoliate, extract, massage, and mask. The main difference is the skincare products used in each facial, and most spas carry at least two lines." },
             { image: spa4, title: "Body Treatments", description: "The basic body scrub exfoliates your outermost, dead skin cells with a salt scrub (rougher), a sugar scrub (gentler), or some other exfoliant, like coffee grounds or fruit enzymes that gently loosen the inter-cellular bonds. " },
         ];
         const details = [
-            { name: staff, description: "Meet our Staff", url: "/about", link: "About Us >>" },
-            { name: products, description: "Beauty Products", url: "/products", link: "Products >>" },
-            { name: reservation, description: "Book an Appointment", url: "/reservation", link: "Reservation >>" }
+            { name: staff, description: "Meet our Staff", url: "/The-Mermaid-Spa-and-Bath/#/about", link: "About Us >>" },
+            { name: products, description: "Beauty Products", url: "/The-Mermaid-Spa-and-Bath/#/products", link: "Products >>" },
+            { name: reservation, description: "Book an Appointment", url: "/The-Mermaid-Spa-and-Bath/#/reservations", link: "Reservation >>" }
         ];
         return (
             <Container fluid className="home-header-padding">
@@ -71,7 +71,9 @@ class Home extends Component {
                                             <div className="home-offerings-normal">
                                                 <div className="home-offerings-text">
                                                     <p style={{ color: "black" }}>{data.description}</p>
-                                                    <a href={data.url} style={{ color: "black" }}>{data.link}</a>
+                                                    <a href={data.url} style={{ color: "black" }}>
+                                                        {data.link}
+                                                    </a>
                                                 </div>
                                             </div>
                                             <div className="home-offerings-hover">
